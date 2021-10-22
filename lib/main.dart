c59f48c48927f646984bd1bcd4de4278e2403344
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-10-22 23:00:54 +0530

    wordpair added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
-        title: 'Welcome to flutter',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to flutter'),
-          ),
-          body: const Center(
-            child: const Text('Hello Pulasthi'),
-          ),
-        ));
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter Pulashi'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
